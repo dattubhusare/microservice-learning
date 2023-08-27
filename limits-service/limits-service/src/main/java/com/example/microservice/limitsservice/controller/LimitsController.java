b9c6bfee800a71c336076152bb0df07bbdf8ec31
dattubhusare <dattu40@gmail.com>
2023-08-27 11:33:39 +0530

    added config class, read properties

--- limits-service/limits-service/src/main/java/com/example/microservice/limitsservice/controller/LimitsController.java
+++ limits-service/limits-service/src/main/java/com/example/microservice/limitsservice/controller/LimitsController.java
@@ -1,17 +1,21 @@
 package com.example.microservice.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservice.limitsservice.beans.Limits;
+import com.example.microservice.limitsservice.config.Configuration;
 
 @RestController
 public class LimitsController {
 	
+	@Autowired
+	private Configuration configuration;
 	@GetMapping("/limits")
 	Limits retriveLimits()
 	{
-		return new Limits(1, 100);
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
 	}
 
 }
